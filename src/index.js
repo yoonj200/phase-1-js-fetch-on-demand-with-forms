2e6a47ae0ba38cecd594fd596f6d004b87aa4556
Joshua <yoonj200@gmail.com>
2022-02-13 19:03:21 -0600

    commit tree d51c595c33e4251c539c51673d56f4fd6ed09b73

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-const init = () => {
-  
+const init = () => { //callback function
+    //add event listener to form element
+    //  event listeners require 2 arguments: 
+    //  event type (string) + listener (callback function)
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //prevents automatic refresh
+        //console.log(event); //confirm everything is working in callback
+        const input = document.querySelector('input#searchByID');
+        
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+           const title = document.querySelector(`section#movieDetails h4`);
+           const summary = document.querySelector(`section#movieDetails p`); 
+            
+           title.innerText = data.title;
+           summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
